cf90dc567adc60d3565e2c0845464071c5916dec
Alvaro <151834557+alanmar5@users.noreply.github.com>
2026-02-13 17:18:15 +0100

    wd

--- consumos_app/settings.py
+++ consumos_app/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-demo-key'
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
